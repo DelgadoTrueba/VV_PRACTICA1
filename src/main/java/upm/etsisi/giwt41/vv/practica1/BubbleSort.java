624a6142bf195084df4af5421c63d1fa9ae4ec50
EOnieva <e.onieva@alumnos.upm.es>
2019-12-13 17:32:03 +0100

    Formato
    Que se vea bonito

--- src/main/java/upm/etsisi/giwt41/vv/practica1/BubbleSort.java
+++ src/main/java/upm/etsisi/giwt41/vv/practica1/BubbleSort.java
@@ -1,32 +1,29 @@
 package upm.etsisi.giwt41.vv.practica1;
 
 public class BubbleSort {
-	/**
-	 * This method the BubbleSort method. Returns
-	 * the array in increasing order.
-	 *
-	 * @param array The array to be sorted
-	 * @param size The count of total number of elements in array
-	 **/
 
-	public static <T extends Comparable<T>> void BS(T array[], int size) {
-		boolean swap;
-		
-		
-		do
-		{
-			swap = false;
-			for (int count = 0; count < size-1; count++)
-			{
-				int comp = array[count].compareTo(array[count + 1]);
-				if (comp > 0)
-				{
-					T temp = array[count];
-					array[count] = array[count + 1];
-					array[count + 1] = temp;
-					swap = true;
-				}
-			}
-		} while (swap);
-	}
+    /**
+     * This method the BubbleSort method. Returns the array in increasing order.
+     *
+     * @param array The array to be sorted
+     * @param size The count of total number of elements in array
+	 *
+     */
+
+    public static <T extends Comparable<T>> void BS(T array[], int size) {
+        boolean swap;
+
+        do {
+            swap = false;
+            for (int count = 0; count < size - 1; count++) {
+                int comp = array[count].compareTo(array[count + 1]);
+                if (comp > 0) {
+                    T temp = array[count];
+                    array[count] = array[count + 1];
+                    array[count + 1] = temp;
+                    swap = true;
+                }
+            }
+        } while (swap);
+    }
 }
